3289f0ac112dbecef0bbb7a9126c6380c74f4850
Jay <jayadityanautiyal07@gmail.com>
2023-02-10 13:32:08 +0530

    added changes

--- testJS.js
+++ testJS.js
@@ -2,7 +2,6 @@ var http = require("http");
 //http server
 http
   .createServer(function (request, response) {
-    // Send the HTTP header
     // HTTP Status: 200 : OK
     // Content Type: text/plain
     response.writeHead(200, { "Content-Type": "text/plain" });
