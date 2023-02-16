661cd1cf7f3c9e04c219a5060c8ed41770df1d38
Jay <jayadityanautiyal07@gmail.com>
2023-02-16 18:34:18 +0530

    file with defects added

--- testJS.js
+++ testJS.js
@@ -1,4 +1,1 @@
-const num3 = 5:
-const num2 = 3;
-const sum = num1 + num2;
-console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    const num3 = 5: const num2 = 3;const sum = num1 + num2;console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
